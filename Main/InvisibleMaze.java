4c016e4367291852bb34594a22e6c02c9d886626
AL <153260270+cikalkautsar@users.noreply.github.com>
2025-05-22 23:47:36 +0700

    Create InvisibleMaze.java

---
+++ Main/InvisibleMaze.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cikalstudio.invisiblemaze;
+
+/**
+ *
+ * @author cikal
+ */
+public class InvisibleMaze {
+
+    public static void main(String[] args) {
+        new Game().start();
+    }
+}
