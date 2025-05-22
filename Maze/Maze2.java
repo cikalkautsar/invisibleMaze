0c0474cbcdad23b7f18de97d23fa6fc842dc7530
AL <153260270+cikalkautsar@users.noreply.github.com>
2025-05-22 23:43:51 +0700

    Create Maze2.java

---
+++ Maze/Maze2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cikalstudio.invisiblemaze;
+
+public class Maze2 extends Maze {
+    @Override
+    public void loadMaze() {
+        System.out.println("==================================================\n" +
+"                INVISIBLE MAZE LEVEL 2                 \n" +
+"==================================================");
+        maze = new char[][] {
+            {'#', '#', '#', '#', '#'},
+            {'#', 'P', ' ', ' ', '#'},
+            {'#', ' ', '#', ' ', '#'},
+            {'#', '#', ' ', '#', '#'},
+            {'#', '#', 'E', '#', '#'}
+        };
+        playerStartX = 1;
+        playerStartY = 1;
+        exitX = 4;
+        exitY = 2;
+    }
+}
