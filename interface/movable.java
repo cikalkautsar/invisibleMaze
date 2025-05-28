5b27b60f548de000eaf4d316e4851bcc8ce2e5c9
AL <153260270+cikalkautsar@users.noreply.github.com>
2025-05-28 23:16:47 +0700

    Update movable.java
    FINALLY ALHAMDULILLAH

--- interface/movable.java
+++ interface/movable.java
@@ -1,6 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ Jadi ini kyk kontrak biar semua objek bisa gerak (move). Semua class yg butuh gerak
+harus implement interface movable ini karena dia punya method move(char direction)
+
  */
 package com.cikalstudio.invisiblemaze;
 
